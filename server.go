cd018f4c7dd11d8344915e240429629308f37b52
l1ttps <32523515+l1ttps@users.noreply.github.com>
2023-10-31 14:10:15 +0700

    feat: create server with configs and driver (#4)

---
+++ server.go
@@ -0,0 +1,52 @@
+package routix
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Controller func() *gin.Engine
+type Middleware func() gin.HandlerFunc
+type ServerConfig struct {
+	Loader      []func()
+	Controllers []Controller
+	Middlewares []gin.HandlerFunc
+	Port        string
+	DebugLogger bool
+}
+
+// CreateServer creates a new Gin server with the given configuration.
+// It takes a ServerConfig parameter that specifies the server's configuration.
+// The function returns a *gin.Engine, which is the created Gin server.
+
+func CreateServer(config ServerConfig) *gin.Engine {
+	if !config.DebugLogger {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	// Create a new Gin server with default middleware
+	Driver = gin.Default()
+
+	// Auto apply global middlewares
+	applyMiddlewares(Driver, config.Middlewares)
+
+	// Connect the controllers to the server
+	for _, controller := range config.Controllers {
+		controller()
+	}
+
+	// Return the created Gin server
+	return Driver
+}
+
+// applyMiddlewares applies a list of middlewares to a gin.Engine.
+//
+// Parameters:
+//   - r: a pointer to a gin.Engine object.
+//   - middlewares: a slice of gin.HandlerFunc objects.
+//
+// Return type: None.
+
+func applyMiddlewares(r *gin.Engine, middlewares []gin.HandlerFunc) {
+	for _, middleware := range middlewares {
+		r.Use(middleware)
+	}
+}
