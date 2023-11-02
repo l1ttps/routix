65ff09e8ac06372e628a7212b2476cdfbd788948
Quang Vinh <l1ttps443@gmail.com>
2023-11-02 12:02:57 +0700

    feat: connect base view dir

--- server.go
+++ server.go
@@ -12,18 +12,22 @@ type ServerConfig struct {
 	Middlewares []gin.HandlerFunc
 	DebugLogger bool
 	PathRoot    string
+	BaseViewDir string
 }
 
 // CreateServer creates a new Gin server with the given configuration.
 // It takes a ServerConfig parameter that specifies the server's configuration.
 // The function returns a *gin.Engine, which is the created Gin server.
 func CreateServer(config ServerConfig) *gin.Engine {
+
 	// Check if debug logger is enabled
 	if !config.DebugLogger {
 		gin.SetMode(gin.ReleaseMode)
 	}
 	// Create a new Gin server with default middleware
 	Driver = gin.Default()
+
+	// Set default base view dir ("/views")
 
 	// Auto apply global middlewares
 	applyMiddlewares(Driver, config.Middlewares)
@@ -35,6 +39,9 @@ func CreateServer(config ServerConfig) *gin.Engine {
 
 	// Connect the controllers to the server
 	connectControllers(config.Controllers)
+
+	// Mapping views folder
+	useBaseViewDir(config.BaseViewDir)
 
 	// Return the created Gin server
 	return Driver
@@ -51,6 +58,7 @@ func applyMiddlewares(r *gin.Engine, middlewares []gin.HandlerFunc) {
 	for _, middleware := range middlewares {
 		r.Use(middleware)
 	}
+	return
 }
 
 // connectControllers connects the given controllers and executes each controller.
@@ -61,4 +69,21 @@ func connectControllers(controllers []ControllerType) {
 	for _, controller := range controllers {
 		controller()
 	}
+	return
+}
+
+// UseBaseViewDir sets the base view directory for loading HTML templates.
+//
+// customBaseViewDir: A string representing a custom base view directory. If
+// provided, the base view directory will be set to this value. Otherwise, the
+// default value "views/*" will be used.
+//
+// No return value.
+func useBaseViewDir(customBaseViewDir string) {
+	if customBaseViewDir != "" && customBaseViewDir != "/" {
+		BaseViewDir = customBaseViewDir
+		Driver.LoadHTMLGlob(BaseViewDir)
+		IsEnableRender = true
+	}
+	return
 }
