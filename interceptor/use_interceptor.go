3312a6d95b5bcefa96d84d14223e673d913068bd
Quang Vinh <l1ttps443@gmail.com>
2023-11-06 09:43:53 +0700

    chore: remove return type for controller

--- interceptor/use_interceptor.go
+++ interceptor/use_interceptor.go
@@ -11,11 +11,16 @@ type InterceptorContext struct {
 // CallHandler is a function type that represents the next function to be called.
 type CallHandler func()
 
-// UseInterceptor is a function that takes an interceptor function and returns a middleware function.
-// The interceptor function takes an InterceptorContext and returns another function.
-// The middleware function takes a *gin.Context and invokes the interceptor function, passing in an InterceptorContext.
-// It then calls the Next() method on the InterceptorContext and invokes the returned function from the interceptor.
-
+// UseInterceptor is a function that takes an interceptor function and returns a gin.HandlerFunc.
+//
+// The interceptor function takes an InterceptorContext pointer as a parameter and returns a function.
+// The InterceptorContext encapsulates the *gin.Context object.
+// The returned function is the next handler to be executed in the middleware chain.
+//
+// The UseInterceptor function itself returns a gin.HandlerFunc, which is a middleware function that conforms to the gin middleware signature.
+// It takes a *gin.Context object as a parameter, creates a new InterceptorContext, and invokes the interceptor function with the InterceptorContext.
+// It then calls the Next() method on the *gin.Context to proceed to the next middleware.
+// Finally, it executes the nextHandler function returned by the interceptor function to execute the interceptor logic after the request is handled by other middlewares.
 func UseInterceptor(interceptorFunc func(c *InterceptorContext) func()) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// Create a new InterceptorContext with the *gin.Context
